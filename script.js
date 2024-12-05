e284ad91178d568c7708580ceb3176be7dab669f
IrvingLB <129083161+IrvingLB@users.noreply.github.com>
2024-12-05 15:25:18 -0600

    Add files via upload
    Ejemplo de portafolio digital.

---
+++ script.js
@@ -0,0 +1,5 @@
+// Script para enviar un mensaje o agregar funcionalidades futuras
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("¡Gracias por contactarme! Pronto responderé a tu mensaje.");
+});
